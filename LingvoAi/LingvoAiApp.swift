ccb2ab61330c0d559e8604e192c5adb1be0124fc
Artur Bagautdinov <arturbagautdinov30@yahoo.com>
2025-07-19 11:24:50 +0300

    Initial Commit

---
+++ LingvoAi/LingvoAiApp.swift
@@ -0,0 +1,17 @@
+//
+//  LingvoAiApp.swift
+//  LingvoAi
+//
+//  Created by Artur Bagautdinov on 19.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct LingvoAiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LingvoAiTests/LingvoAiTests.swift
@@ -0,0 +1,17 @@
+//
+//  LingvoAiTests.swift
+//  LingvoAiTests
+//
+//  Created by Artur Bagautdinov on 19.07.2025.
+//
+
+import Testing
+@testable import LingvoAi
+
+struct LingvoAiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
