25e9eed8e30f5ea9c9e0e7d7a0963caadcf74198
lajsan <lajsanminlebaeva@mail.ru>
2025-07-21 16:25:51 -0700

    validationMessages

---
+++ LingvoAi/Profile/ProfileCreationView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileCreationView.swift
+//  LingvoAi
+//
+//  Created by Ляйсан on 21.07.2025.
+//
+
+import SwiftUI
+
+struct ProfileCreationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileCreationView()
+}
